50b02f5eea5966cab36a088c8d1f8cc552cdf807
charleytian-maker <charleytian@gmail.com>
2026-06-21 22:40:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-<!-- 这里是您网页原本的其他内容 -->
-
-    <!-- 1. 刚才为您编写的弹窗 HTML 结构（如果还没加，可以和脚本放在一起） -->
-    <div id="imageModal" class="modal" onclick="closeModal()">
+  <div id="imageModal" class="modal" onclick="closeModal()">
       <div class="modal-content" onclick="event.stopPropagation()">
         <span class="close-btn" onclick="closeModal()">&times;</span>
-        < img id="modalImg" src="" alt="放大图片">
+        <img id="modalImg" src="" alt="放大图片">
         <div id="modalDescription" class="modal-desc"></div>
       </div>
     </div>
@@ -21,7 +18,7 @@
         modalImg.src = imgElement.src; // 放大图片路径等于当前点击的图片
         
         // 设置下方产品描述：使用图片的 alt 属性文字 + 自定义介绍
-        modalDesc.innerText = imgElement.alt + " - 这是一款高品质的专业匹克球套装，适合各阶段玩家。"; 
+        modalDesc.innerText=imgElement.alt + " - 这是一款高品质的专业匹克球套装，适合各阶段玩家。"; 
       }
 
       // 关闭弹窗的函数
